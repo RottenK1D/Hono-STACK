b07d2204dee66829161e8756ef251f6284fea390
Rinards Sondors <rinards.sondors@gmail.com>
2023-11-19 19:05:26 +0000

    YES

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import { Hono } from "hono";
 import { renderer } from "./renderer";
-import { html } from "hono/html";
 
 const app = new Hono();
 
 app.get("*", renderer);
 
 app.get("/", (c) => {
-	return c.render(<h1 class="font:50 italic fg:red">Hello!</h1>);
+	return c.render(
+		<div hx-post="/clicked" hx-swap="outerHTML">
+			Click me
+		</div>,
+	);
 });
+
+app.post("/clicked", (c) => c.render(<div>Hello FROM HTMX</div>));
 
 export default app;
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,6 +1,5 @@
 import "hono";
 import { jsxRenderer } from "hono/jsx-renderer";
-import { html } from "hono/html";
 
 declare module "hono" {
 	interface ContextRenderer {
@@ -9,28 +8,9 @@ declare module "hono" {
 }
 
 export const renderer = jsxRenderer(
-	({ children, title }) => {
+	({ children }) => {
 		return (
-			<html lang="en" style="display:none">
-				<head>
-					{html`
-            <link rel="preload" as="style" href="https://esm.sh/@master/normal.css@beta?css">
-            <link rel="stylesheet" href="https://esm.sh/@master/normal.css@beta?css">
-            <link rel="modulepreload" href="https://esm.sh/@master/css@beta">
-            <script type="module">
-              import { htmx } from "https://esm.sh/htmx.org@1.9.8"
-            </script>
-            <script type="module">
-              import { initRuntime } from 'https://esm.sh/@master/css@beta'
-              initRuntime({
-                variables: {
-                  primary: '#000000'
-                }
-              })
-            </script>
-          `}
-					<title>{title}</title>
-				</head>
+			<html lang="en">
 				<body>{children}</body>
 			</html>
 		);
